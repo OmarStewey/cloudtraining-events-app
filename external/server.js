8a9bc10e99a2c9c59013e6f7af2d3e5730e23e40
Omar Stewart <omarstewey@outlook.com>
2022-10-06 15:11:11 +0000

    My first change

--- external/server.js
+++ external/server.js
@@ -174,6 +174,7 @@ const server = app.listen(SERVICE_PORT, () => {
     const host = server.address().address;
     const port = server.address().port;
 
+    console.log('Cloud Cocktail!')
     console.log(`Events app listening at http://${host}:${port}`);
 });
 
