81e0f4a1a1534f79cd1bb914446ffff434bdfec2
Omar Stewart <omarstewey@outlook.com>
2022-10-07 16:56:00 +0000

    fix homepage test

--- external/test/test-server.js
+++ external/test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes("Cloud Cocktail"));
         return done();
       });
 
